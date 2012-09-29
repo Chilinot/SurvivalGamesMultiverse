296db36303e56df77ec19a6368429bb9c9e09c13
Lucas Arnström <lucasarnstrom@gmail.com>
2012-09-29 18:00:52 +0200

    code formatted
    Signed-off-by: Lucas Arnström <lucasarnstrom@gmail.com>

--- src/me/lucasemanuel/survivalgamesmultiverse/utils/WorldGuardHook.java
+++ src/me/lucasemanuel/survivalgamesmultiverse/utils/WorldGuardHook.java
@@ -25,44 +25,44 @@ import com.sk89q.worldguard.protection.managers.RegionManager;
 import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 
 public class WorldGuardHook {
-	
+
 	public static boolean isInRegion(Location playerlocation, String regionname) {
-		
+
 		if (regionname == null) {
-            return true;
-        }
-        ApplicableRegionSet set = getWGSet(playerlocation);
-        if (set == null) {
-            return false;
-        }
-        for (ProtectedRegion r : set) {
-            if (r.getId().equalsIgnoreCase(regionname)) {
-                return true;
-            }
-        }
-        return false;
+			return true;
+		}
+		ApplicableRegionSet set = getWGSet(playerlocation);
+		if (set == null) {
+			return false;
+		}
+		for (ProtectedRegion r : set) {
+			if (r.getId().equalsIgnoreCase(regionname)) {
+				return true;
+			}
+		}
+		return false;
 	}
-	
+
 	private static ApplicableRegionSet getWGSet(Location loc) {
-        WorldGuardPlugin wg = getWorldGuard();
-        if (wg == null) {
-            return null;
-        }
-        RegionManager rm = wg.getRegionManager(loc.getWorld());
-        if (rm == null) {
-            return null;
-        }
-        return rm.getApplicableRegions(com.sk89q.worldguard.bukkit.BukkitUtil.toVector(loc));
-    }
- 
-    public static WorldGuardPlugin getWorldGuard() {
-        Plugin plugin = Bukkit.getServer().getPluginManager().getPlugin("WorldGuard");
- 
-        // WorldGuard may not be loaded
-        if (plugin == null || !(plugin instanceof WorldGuardPlugin)) {
-            return null;
-        }
-        return (WorldGuardPlugin) plugin;
-    }
+		WorldGuardPlugin wg = getWorldGuard();
+		if (wg == null) {
+			return null;
+		}
+		RegionManager rm = wg.getRegionManager(loc.getWorld());
+		if (rm == null) {
+			return null;
+		}
+		return rm.getApplicableRegions(com.sk89q.worldguard.bukkit.BukkitUtil.toVector(loc));
+	}
+
+	public static WorldGuardPlugin getWorldGuard() {
+		Plugin plugin = Bukkit.getServer().getPluginManager().getPlugin("WorldGuard");
+
+		// WorldGuard may not be loaded
+		if (plugin == null || !(plugin instanceof WorldGuardPlugin)) {
+			return null;
+		}
+		return (WorldGuardPlugin) plugin;
+	}
 
 }
