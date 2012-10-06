20fd52ca62fee30454aeb33ddc4baed039d18d27
Lucas Arnström <lucasarnstrom@gmail.com>
2012-10-06 12:46:09 +0200

    cleanup
    Signed-off-by: Lucas Arnström <lucasarnstrom@gmail.com>

--- src/me/lucasemanuel/survivalgamesmultiverse/managers/StatusManager.java
+++ src/me/lucasemanuel/survivalgamesmultiverse/managers/StatusManager.java
@@ -19,7 +19,6 @@ import java.util.HashMap;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.World;
 import org.bukkit.entity.Player;
 
 import me.lucasemanuel.survivalgamesmultiverse.Main;
